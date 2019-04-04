a2f7b695df90cf22e55d6e0a4acfd5d3e7cb537f
Johnny Lim <bluejayl@ucla.edu>
2019-04-03 23:53:07 -0700

    added more elephants and sound on click

--- main.js
+++ main.js
@@ -17,12 +17,23 @@ document.body.onclick = function(e) {
   console.log(e.pageX)
   console.log(e.pageY)
 
+let arrElephants = ['elephant-final.png','ele2.png','ele3.png','ele4.png', 'ele5.png','ele6.png','ele7.png']
+let index = Math.floor(Math.random() * 7);
+let randomElephant = arrElephants[index];
   elephant = document.createElement('img');
-  let imgURL = true ? chrome.extension.getURL('elephant-final.png') : 'chrome-extension/elephant-final.png'
+
+  let imgURL = true ? chrome.extension.getURL(randomElephant) : `chrome-extension/${randomElephant}`
   elephant.setAttribute("src", imgURL);
   elephant.setAttribute("style", "position:absolute; display:block; left:" + e.pageX + "px; top:" + e.pageY + "px; width: 100px; height: 75px; z-index:1000;");
 
   console.log(elephant)
+
+  let audioElement = document.createElement('audio');
+  audioElement.setAttribute('src', chrome.extension.getURL('elephant_sound.mp3'));
+  audioElement.play();
+
+
+  
 
 
   document.body.appendChild(elephant)
